aac230ca19918b6e733f4b152a7e9db46f822c34
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2022-11-02 13:09:59 -0400

    Initial commit

---
+++ DSA_Masterclass/countUniqueValues.js
@@ -0,0 +1,30 @@
+//use multiple pointers
+// 0(n) time complexity | 0(1) space complexity
+
+function countUniqueValues(array){
+    let current = 0;
+    let scout = 1;
+    if (array.length === 0) {
+        return 0;
+    }
+    while (scout < array.length) {
+        // if left and scout are equal increment j
+        if (array[current] === array[scout]) {
+            scout++;
+        //if they are not set array[current] = scout
+        //increment current by 1
+        } else {
+            current++;
+            array[current] = array[scout]
+        }
+    }
+    //return count + 1 to correct for zero index
+    return current + 1;
+};
+
+console.log(countUniqueValues([1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 5])); //5
+console.log(countUniqueValues([1, 1, 3, 4, 5])); //4
+console.log(countUniqueValues([1, 1, 1, 4, 5])); //3
+console.log(countUniqueValues([1, 1, 1, 1, 5])); //2
+console.log(countUniqueValues([1, 1, 1, 1, 1])); //1
+console.log(countUniqueValues([-1, -2, 0, 4, 5, 5, 5, 5, 7, 7, 8, 8, 9])); //8
---
+++ DSA_Masterclass/validAnagram.js
@@ -0,0 +1,30 @@
+// Frequency Counter - validAnagram
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+// Examples:
+
+// validAnagram('', '') // true
+// validAnagram('aaz', 'zza') // false
+// validAnagram('anagram', 'nagaram') // true
+// validAnagram("rat","car") // false) // false
+// validAnagram('awesome', 'awesom') // false
+// validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+// validAnagram('qwerty', 'qeywrt') // true
+// validAnagram('texttwisttime', 'timetwisttext') // true
+// Note: You may assume the string contains only lowercase alphabets.
+
+// runs 0(n log n) because of the sort method)
+
+function validAnagram(string1, string2) {
+    //sort each word and compare the sort
+    return string1.split('').sort().join() === string2.split('').sort().join()
+}
+
+console.log(validAnagram('', '')) // true
+console.log(validAnagram('aaz', 'zza')) // false
+console.log(validAnagram('anagram', 'nagaram') )// true
+console.log(validAnagram("rat","car")) // false
+console.log(validAnagram('awesome', 'awesom')) // false
+console.log(validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana')) // false
+console.log(validAnagram('qwerty', 'qeywrt')) // true
+console.log(validAnagram('texttwisttime', 'timetwisttext')) // true
