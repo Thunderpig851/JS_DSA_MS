6fa54229f329622acce8c7fa99edec721b608e9c
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2022-12-13 13:00:40 -0500

    insetion sort makes sense now.

--- Sorting/insertionSort.js
+++ Sorting/insertionSort.js
@@ -1,3 +1,20 @@
 function insertionSort(array) {
-    
-}
+    let holePosition;
+    let valueToInsert;
+
+    for (let i = 0; i < array.length; i ++) {
+        holePosition = i;
+        valueToInsert = array[i];
+
+        while (holePosition > 0 && array[holePosition - 1] > valueToInsert) {
+            array[holePosition] = array[holePosition - 1];
+            holePosition --;
+        }
+        array[holePosition] = valueToInsert;
+        console.log(array);
+    }
+    return array;
+};
+
+
+console.log(insertionSort([34, 22, 10, 19, 17]));
