4c7ab9c2260d3f39aba9aa426fbb7ed64c9ef65b
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2023-01-04 13:02:37 -0500

    DBL LL Completed

--- Data_Structures/doublyLinkedList.js
+++ Data_Structures/doublyLinkedList.js
@@ -203,14 +203,5 @@ class DoublyLinkedList {
 }
 
 const list = new DoublyLinkedList();
-list.push(0);
-list.push(1);
-list.push(2)
-list.push(3);
-list.push(4);
-list.push(5);
-list.push(6);
-list.push(7);
-list.push(8);
-list.remove(4);
+list.push(0).push(1).push(2).push(3);
 list.traverse();
