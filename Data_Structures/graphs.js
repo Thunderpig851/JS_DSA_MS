3851eac7960eecca35cde846618668909c875549
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2023-01-10 16:18:58 -0500

    removeEdge complete

--- Data_Structures/graphs.js
+++ Data_Structures/graphs.js
@@ -5,14 +5,25 @@ class Graph {
 
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+        return this;
     }
 
     addEdge(vertex1, vertex2) {
-
         if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
             this.adjacencyList[vertex1].push(vertex2);
             this.adjacencyList[vertex2].push(vertex1);
         }
+        return this;
+    }
+
+    removeEdge(vertex1, vertex2) {
+        if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+            let edge1 = this.adjacencyList[vertex1].indexOf(vertex2);
+            let edge2 = this.adjacencyList[vertex2].indexOf(vertex1);
+            this.adjacencyList[vertex1].splice(edge1, 1);
+            this.adjacencyList[vertex2].splice(edge2, 1);
+        }
+        return this;
     }
 }
 
@@ -20,4 +31,5 @@ const graph = new Graph();
 graph.addVertex('Tokyo');
 graph.addVertex('Berlin');
 graph.addEdge('Tokyo', 'Berlin')
+graph.removeEdge('Tokyo', 'Berlin')
 console.log(graph.adjacencyList)
