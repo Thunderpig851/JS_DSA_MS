3653ffc3bb547464f9bde49f1cf94f793816d446
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2023-01-18 13:20:42 -0500

    isValid Complete in 0(n) time in JS

---
+++ Grind_75/isValid.js
@@ -0,0 +1,28 @@
+var isValid = function(string) {
+    let stack = [];
+
+    for (let i = 0; i < string.length; i ++) {
+        let char = string[i];
+
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char); 
+        } else if (char === ')' && stack[stack.length - 1] === '(') {
+            stack.pop();
+        } else if (char === ']' && stack[stack.length - 1] === '[') {
+            stack.pop();
+        } else if (char === '}' && stack[stack.length - 1] === '{') {
+            stack.pop();
+        }  else if ((char === ')' || char === '}' || char === ']') && (stack.length === 0)) {
+            return false;
+        } else if (stack !== char) {
+            return false
+        }
+
+    }
+    return stack.length === 0
+};
+console.log(isValid('(])'))
+console.log(isValid('()')) // true
+console.log(isValid('([)]')) // false
+console.log(isValid('((()()((')) // False
+console.log(isValid('()())))')) // false
