3d1a32a0f682de14dda3fc8542ad5be0b693ed3d
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2023-02-01 17:05:42 -0500

    progress on rotting roanges

--- Grind_75/rottenOranges.js
+++ Grind_75/rottenOranges.js
@@ -20,78 +20,47 @@ var orangesRotting = function(grid) {
     }
     
     while (queue.length) {
-        console.log("GRID: ")
-        grid.forEach(row => {
-        console.log(row)
-        })
-        // console.log("QUEUE: ")
-        // queue.forEach(item => {
-        // console.log(item)
-        // })
         const [i, j] = queue.shift();
         let k = queue.length;
+        let flag = false;
         while ( k >= 0) {
             if (j + 1 < grid[i].length && grid[i][j + 1] === 1) {
+                flag = true;
                 count ++;
                 grid[i][j + 1] = 2;
                 queue.push([i, j + 1]);
             }
             if (i + 1 < grid.length && grid[i + 1][j] === 1) {
+                flag = true;
                 count ++;
                 grid[i + 1][j] = 2;
                 queue.push([i + 1, j]);
             }
             if (j - 1 >= 0 && grid[i][j - 1] === 1) {
+                flag = true;
                 count ++;
                 grid[i][j - 1];
                 queue.push([i, j - 1]);
             }
             if (i - 1 >= 0 && grid[i - 1][j] === 1) {
+                flag = true;
                 count ++;
                 grid[i - 1][j] = 2;
                 queue.push([i - 1, j]);
             }
             k --;
         }
-        if (queue.length) minutes ++;
+        console.log(minutes)
+        if (flag) {
+            minutes ++;
+            flag = false;
+        }
     }
     
     return count === totalOranges ? minutes : -1;
 };
-//console.log(orangesRotting([[1, 2]])); // 1
-//console.log(orangesRotting([[2,1,1],[1,1,0],[0,1,1]])) // 4 
-console.log(orangesRotting([[2,1,1],[0,1,1],[1,0,1],[2,0,0]])); // -1
+// console.log(orangesRotting([[1, 2]])); // 1
+// console.log(orangesRotting([[2,1,1],[1,1,0],[0,1,1]])) // 4 
+// console.log(orangesRotting([[2,1,1],[0,1,1],[1,0,1]])); // -1
+console.log(orangesRotting([[2,0,1,1,1,1,1,1,1,1],[1,0,1,0,0,0,0,0,0,1],[1,0,1,0,1,1,1,1,0,1],[1,0,1,0,1,0,0,1,0,1],[1,0,1,0,1,0,0,1,0,1],[1,0,1,0,1,1,0,1,0,1],[1,0,1,0,0,0,0,1,0,1],[1,0,1,1,1,1,1,1,0,1],[1,0,0,0,0,0,0,0,0,1],[1,1,1,1,1,1,1,1,1,1]]))
 
-// 211
-// 011
-// 101
-
-//const BFS = (i, j) => {
-    //     const queue = [[i, j]];
-    //     while (queue.length) {
-            
-
-    //         const [i, j] = queue.shift();
-
-    //         if (i + 1 < grid.length && grid[i + 1][j] === 1) {
-    //             grid[i + 1][j] = 2
-    //             queue.push([i + 1, j])
-    //         }
-    //         if (j + 1 < grid[i].length && grid[i][j + 1] === 1) {
-    //             grid[i][j + 1] = 2
-    //             queue.push([i, j + 1])
-    //         }
-    //         if (i - 1 >= 0 && grid[i - 1][j] === 1) {
-    //             queue.push([i - 1, j])
-    //         }
-    //         if (j - 1 >= 0 && grid[i][j - 1] === 1) {
-    //             queue.push([i, j - 1])
-    //         }
-
-    //         // console.log("GRID: ")
-    //         // grid.forEach(row => {
-    //         //     console.log(row)
-    //         // })
-
-    //     }
-    // };
