2413e59759dba293bdd8f2a65d9b14d9ec0d67d2
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2023-02-06 13:56:55 -0500

    progress with improving longestPalindromes time complexity

--- Grind_75/palindromeSubString.js
+++ Grind_75/palindromeSubString.js
@@ -1,32 +1,23 @@
+/*
+Time Complexity: O(n^3)
+*/
 var longestPalindrome = function(string) {
-    let maxLength = 0;
-    let maxPal = '';
+    if (string.length < 1) return "";
 
+    let maxSubStart = 0;
+    let maxSubLength = 0;
     for (let i = 0; i < string.length; i ++) {
-        let substring = string.substring(i, string.length);
         
-        for (let j = substring.length; j >= 0; j --) {
-            let subSubString = substring.substring(0, j);
-
-            if (isPal(subSubString)) {
-                if (subSubString.length > maxLength) {
-                    maxLength = subSubString.length;
-                    maxPal = subSubString;
-                }
-            }
-        }
     }
-    
-    return maxPal;
 };
 
 // Helper function to check
-const isPal = (string) => {
-    let reverse = '';
-    for (let i = string.length - 1; i >= 0; i --) {
-        reverse += string[i];
-    }
-    return string === reverse;
+const expandAroundCenter = (string, left, right) => {
+   while (left >= 0 && right < string.length - 1 && string[left] === string[right]) {
+    left --;
+    right ++
+   }
+   return 
 };
 
 console.log(longestPalindrome('babad'));
